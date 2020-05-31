d919127a9f4c03c536350c7e35df38924cce1d34
Oliver Joos <oliver.joos@hispeed.ch>
2020-05-31 15:24:11 +0200

    Add config of external SPI flash (tested with 16MiB on WeAct V2.0)

--- ports/stm32/boards/WeAct_F411CE/board_init.c
+++ ports/stm32/boards/WeAct_F411CE/board_init.c
@@ -1,6 +1,9 @@
 #include "py/mphal.h"
 
-void WeAct_Core_board_early_init(void) {
-    mp_hal_pin_output(pin_A4);
-    mp_hal_pin_write(pin_A4, 1);
+void WeAct_F411CE_board_early_init(void) {
+#ifdef MICROPY_HW_SPIFLASH_CS
+    // Set SPI flash CS pin to output and high
+    mp_hal_pin_output(MICROPY_HW_SPIFLASH_CS);
+    mp_hal_pin_write(MICROPY_HW_SPIFLASH_CS, 1);
+#endif
 }
