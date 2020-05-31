1bb2857b7379bfa0f83af20425f50b6909b6f00c
Oliver Joos <oliver.joos@hispeed.ch>
2020-05-31 16:01:36 +0200

    Add/fix comments. Update pinout in CubeMX project.

--- ports/stm32/boards/WeAct_F411CE/mpconfigboard.h
+++ ports/stm32/boards/WeAct_F411CE/mpconfigboard.h
@@ -13,8 +13,9 @@
 #define MICROPY_HW_ENABLE_SDCARD    (0)
 #define MICROPY_HW_ENABLE_RNG       (0)     // no hardware random gen. available on F401 and F411
 
-// 1 = use internal flash (512 KByte)
-// 0 = use onboard SPI flash (set correct size below!)
+
+// 1 = Use internal flash (64 of 512 KByte)
+// 0 = Use onboard SPI flash (set correct size below!)
 #define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (0)
 
 #if !MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE
@@ -52,7 +53,7 @@ extern struct _spi_bdev_t spi_bdev;
 
 #endif  // !MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE
 
-// HSE is 8MHz, CPU freq set to 96MHz
+// HSE is 25MHz, CPU freq set to 96MHz
 #define MICROPY_HW_CLK_PLLM (25)
 #define MICROPY_HW_CLK_PLLN (192)
 #define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV2)
@@ -73,10 +74,12 @@ extern struct _spi_bdev_t spi_bdev;
 // UART config
 #define MICROPY_HW_UART1_TX     (pin_A9)
 #define MICROPY_HW_UART1_RX     (pin_A10)
-// #define MICROPY_HW_UART2_CTS    (pin_A0)
-// #define MICROPY_HW_UART2_RTS    (pin_A1)
+
 #define MICROPY_HW_UART2_TX     (pin_A2)
 #define MICROPY_HW_UART2_RX     (pin_A3)
+// #define MICROPY_HW_UART2_CTS    (pin_A0)     // CTS conflicts with USRSW
+// #define MICROPY_HW_UART2_RTS    (pin_A1)
+
 // UART 1 connects to the STM32F411 on the WeAct board
 // and this is exposed as a USB Serial port.
 #define MICROPY_HW_UART_REPL        PYB_UART_1
@@ -90,39 +93,45 @@ extern struct _spi_bdev_t spi_bdev;
 #define MICROPY_HW_I2C3_SCL (pin_A8)
 #define MICROPY_HW_I2C3_SDA (pin_B8)
 
-// SPI busses
+// SPI1 may be used if no SPI flash is mounted
 #define MICROPY_HW_SPI1_NSS     (pin_A4)
 #define MICROPY_HW_SPI1_SCK     (pin_A5)
 #define MICROPY_HW_SPI1_MOSI    (pin_A7)
 #define MICROPY_HW_SPI1_MISO    (pin_B4)
 
+// SPI2 may be used if SDCard interface is disabled
+#if !MICROPY_HW_ENABLE_SDCARD
 #define MICROPY_HW_SPI2_NSS     (pin_B12)
 #define MICROPY_HW_SPI2_SCK     (pin_B13)
 #define MICROPY_HW_SPI2_MISO    (pin_B14)
 #define MICROPY_HW_SPI2_MOSI    (pin_B15)
+#endif
 
-#define MICROPY_HW_SPI3_NSS     (pin_A15)
-#define MICROPY_HW_SPI3_SCK     (pin_B3)
-#define MICROPY_HW_SPI3_MISO    (pin_B4)
-#define MICROPY_HW_SPI3_MOSI    (pin_B5)
+// SPI3 MISO conflicts with SPI1 and SPI2
+// #define MICROPY_HW_SPI3_NSS     (pin_A15)
+// #define MICROPY_HW_SPI3_SCK     (pin_B12)
+// #define MICROPY_HW_SPI3_MISO    (pin_B4)
+// #define MICROPY_HW_SPI3_MOSI    (pin_B5)
 
+// SPI4 conflicts with USB Device Mode
 // #define MICROPY_HW_SPI4_NSS     (pin_B12)
 // #define MICROPY_HW_SPI4_SCK     (pin_B13)
 // #define MICROPY_HW_SPI4_MISO    (pin_A1)
 // #define MICROPY_HW_SPI4_MOSI    (pin_A11)
 
+// SPI5 conflicts with USB Device Mode
 // #define MICROPY_HW_SPI5_NSS     (pin_B1)
 // #define MICROPY_HW_SPI5_SCK     (pin_A10)
 // #define MICROPY_HW_SPI5_MISO    (pin_A12)
 // #define MICROPY_HW_SPI5_MOSI    (pin_B0)
 
-// USRSW is pulled low. Pressing the button makes the input go high.
+// USRSW is pulled up. Pressing the button makes the input go low.
 #define MICROPY_HW_USRSW_PIN        (pin_A0)
 #define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
 #define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
 #define MICROPY_HW_USRSW_PRESSED    (0)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_C13) // Blue C13 LED on board
+#define MICROPY_HW_LED1             (pin_C13)   // Blue C13 LED on board
 #define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_low(pin))
 #define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_high(pin))
