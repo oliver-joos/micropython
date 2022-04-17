a5a7735bb50d7bb24a234f084db577841cfcb061
Oliver Joos <oliver.joos@hispeed.ch>
2022-04-17 23:58:29 +0200

    Remove board_init() and pins that do not exist on a STM32F411CE

--- ports/stm32/boards/WeAct_F411CE/board_init.c
+++
@@ -1,9 +0,0 @@
-#include "py/mphal.h"
-
-void WeAct_F411CE_board_early_init(void) {
-#ifdef MICROPY_HW_SPIFLASH_CS
-    // Set SPI flash CS pin to output and high
-    mp_hal_pin_output(MICROPY_HW_SPIFLASH_CS);
-    mp_hal_pin_write(MICROPY_HW_SPIFLASH_CS, 1);
-#endif
-}
