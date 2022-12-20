8dcf91b9d5c2aa39deadf89471fae85990ab83ee
Oliver Joos <oliver.joos@feller.ch>
2022-12-20 13:55:25 +0100

    py/mpprint: Handle format strings with length specifiers "h" or "hh".
    This commit fixes log messages from submodule lwip that are printed when debug mode is enabled with "#define LWIP_DEBUG 1".
    
    Signed-off-by: Oliver Joos <oliver.joos@hispeed.ch>

--- py/mpprint.c
+++ py/mpprint.c
@@ -511,6 +511,11 @@ int mp_vprintf(const mp_print_t *print, const char *fmt, va_list args) {
                 long_long_arg = true;
             }
             #endif
+        } else if (*fmt == 'h') {
+            ++fmt;
+            if (*fmt == 'h') {
+                ++fmt;
+            }
         }
 
         if (*fmt == '\0') {
